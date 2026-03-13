5d98c1600d94953dc6eb540c4711233d34a5d698
Krishnak101 <kunatibala101@gmail.com>
2026-03-12 20:10:31 -0400

    Products Display handled (FrontEnd)

--- frontend/estore/src/app/components/home/home.ts
+++ frontend/estore/src/app/components/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { CategoryNavigation } from './category.navigation/category.navigation';
 import { SideNavigation } from './side-navigation/side-navigation';
+import { Products } from "../products/products";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, CategoryNavigation, SideNavigation],
+  imports: [Header, CategoryNavigation, SideNavigation, Products],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
