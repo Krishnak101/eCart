0dbd64cd98bed1fa2bdbd2a4eb04a500c91fda40
Krishnak101 <kunatibala101@gmail.com>
2026-03-15 18:37:57 -0400

    Fixed CORS issue and supported api filtering using ParentId (Backend)

--- backend/ecart/src/main/java/com/krishna/ecart/config/MyDataRestConfig.java
+++ backend/ecart/src/main/java/com/krishna/ecart/config/MyDataRestConfig.java
@@ -17,6 +17,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer{
 		// TODO Auto-generated method stub
 		RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
 		
+		cors.addMapping("/api/**")
+        .allowedOrigins("http://localhost:4200") // Or "*" to allow all requests
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("*");
 		HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 		
 		config.getExposureConfiguration().forDomainType(Product.class).withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions)).withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
--- backend/ecart/src/main/java/com/krishna/ecart/dao/ProductRepository.java
+++ backend/ecart/src/main/java/com/krishna/ecart/dao/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.krishna.ecart.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.krishna.ecart.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	//  find Products where Category.id equals (?)
+	Page<Product> findByCategoryId(@Param("id")int categoryId, Pageable pageable);
+	Page<Product> findByProductName(@Param("name")String productname, Pageable pageable);
+//  find Products where Category.parentCategoryId equals (?)
+	Page<Product> findByCategoryParentCategoryId(@Param("id")int parentCategoryId, Pageable pageable);
 
 }
