6fcea27c6575a09ee2fcb8914adce3627e5cf5fb
Krishnak101 <kunatibala101@gmail.com>
2026-03-15 23:34:26 -0400

    Handled Search Feature for Empty Products List (Frontend)

--- frontend/estore/src/app/components/products/products.ts
+++ frontend/estore/src/app/components/products/products.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Ratings } from '../ratings/ratings';
 import { ProductsStore } from './../home/services/product/products-store';
+import { faBoxOpen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule, Ratings],
+  imports: [CommonModule, Ratings, FontAwesomeModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
 export class Products {
+  faBoxOpen = faBoxOpen;
   constructor(public productsStore: ProductsStore) {}
 }
