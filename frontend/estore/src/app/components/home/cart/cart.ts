8daf6cb5aabae62f9cc22df5cb27e1687888eb51
Krishnak101 <kunatibala101@gmail.com>
2026-03-19 19:48:56 -0400

    Handled the Cart Page View (Frontend)

--- frontend/estore/src/app/components/home/cart/cart.ts
+++ frontend/estore/src/app/components/home/cart/cart.ts
@@ -5,10 +5,11 @@ import { CartItem } from '../types/cart-type';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBoxOpen, faShoppingCart, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
+import { Ratings } from "../../ratings/ratings";
 
 @Component({
   selector: 'app-cart',
-  imports: [FontAwesomeModule, CommonModule],
+  imports: [FontAwesomeModule, CommonModule, Ratings],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
